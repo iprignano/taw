fb66bd9ef47f88d72864a2be000d3cbf58ca6237
Ivan Prignano <ivanprignano@gmail.com>
2025-10-22 12:27:19 +0200

    move drumming sequencing logic into DrumsSequencer

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,67 +1,15 @@
-import { fill } from 'es-toolkit/array';
-import { createEffect, createSignal, onCleanup } from 'solid-js';
-import { createStore } from 'solid-js/store';
+import { createSignal } from 'solid-js';
 
-import { getAudioContext, playSnare, playKick, playHihats } from '../../lib/audio';
 import Header from '../Header/Header';
 import DrumsSequencer from '../DrumsSequencer/DrumsSequencer';
 import Keyboard from '../Keyboard/Keyboard';
 import Footer from '../Footer/Footer';
-import type { OnStepToggle } from '../DrumsSequencer/DrumsSequencer';
 
 import styles from './styles.module.css';
 
 export default function App() {
-  const initialDrumsStore = {
-    kick: fill(Array(16), false),
-    snare: fill(Array(16), false),
-    hihats: fill(Array(16), false),
-  };
-  const [step, setStep] = createSignal(0);
   const [bpm, setBpm] = createSignal(120);
-  const [intervalId, setIntervalId] = createSignal<NodeJS.Timeout>();
   const [isPlaying, setIsPlaying] = createSignal(false);
-  const [drumsStore, setDrumsStore] = createStore(initialDrumsStore);
-
-  const onStepToggle: OnStepToggle = (instrument, step, isChecked) => {
-    setDrumsStore(instrument, step - 1, isChecked);
-  };
-
-  createEffect(() => {
-    if (!isPlaying()) {
-      return;
-    }
-
-    const interval = setInterval(() => {
-      if (step() >= 16) {
-        setStep(0);
-      }
-
-      const time = getAudioContext().currentTime;
-      if (drumsStore.kick.at(step() + 1)) {
-        playKick(time);
-      }
-      if (drumsStore.snare.at(step() + 1)) {
-        playSnare(time);
-      }
-      if (drumsStore.hihats.at(step() + 1)) {
-        playHihats(time);
-      }
-
-      setStep((step) => step + 1);
-    }, 60_000 / bpm() / 4);
-    // 1 minute is 60_000
-    // 60_000 / 4 gives us the interval between quarter notes
-    // we divide by 4 because we want 16th notes
-
-    setIntervalId(interval);
-
-    // Clean up any running timer when the BPM
-    // changes or the playback is stopped
-    onCleanup(() => {
-      clearInterval(intervalId());
-    });
-  });
 
   return (
     <div class={styles.wrapper}>
@@ -72,12 +20,7 @@ export default function App() {
           <div class={styles.title}>
             <span class="monospace">drums</span>
           </div>
-          <DrumsSequencer
-            activeStep={step()}
-            isPlaying={isPlaying()}
-            drumsStore={drumsStore}
-            onStepToggle={onStepToggle}
-          />
+          <DrumsSequencer isPlaying={isPlaying()} bpm={bpm()} />
         </div>
         <div class={`${styles.keyboard} ${styles.instrument}`}>
           <div class={styles.title}>
--- src/components/DrumsSequencer/DrumsSequencer.tsx
+++ src/components/DrumsSequencer/DrumsSequencer.tsx
@@ -1,24 +1,72 @@
 import styles from './styles.module.css';
-import type { Store } from 'solid-js/store';
+import { fill } from 'es-toolkit/array';
+import { createEffect, createSignal, onCleanup } from 'solid-js';
+import { createStore } from 'solid-js/store';
+
+import { getAudioContext, playSnare, playKick, playHihats } from '../../lib/audio';
 
 const STEPS_LENGHT = 16;
 const STEPS_ARRAY = Array.from({ length: STEPS_LENGHT }, (_, i) => i + 1);
 const INSTRUMENTS = ['kick', 'snare', 'hihats'] as const;
 
 type Instrument = (typeof INSTRUMENTS)[number];
-export type OnStepToggle = (instrument: Instrument, step: number, isChecked: boolean) => void;
+type OnStepToggle = (instrument: Instrument, step: number, isChecked: boolean) => void;
 
-export default function DrumsSequencer(props: {
-  activeStep: number;
-  drumsStore: Store<any>;
-  onStepToggle: OnStepToggle;
-  isPlaying: boolean;
-}) {
+export default function DrumsSequencer(props: { bpm: number; isPlaying: boolean }) {
+  const initialDrumsStore = {
+    kick: fill(Array(16), false),
+    snare: fill(Array(16), false),
+    hihats: fill(Array(16), false),
+  };
+  const [currentStep, setCurrentStep] = createSignal(0);
+  const [intervalId, setIntervalId] = createSignal<NodeJS.Timeout>();
+  const [drumsStore, setDrumsStore] = createStore(initialDrumsStore);
+
+  const onStepToggle: OnStepToggle = (instrument, step, isChecked) => {
+    setDrumsStore(instrument, step - 1, isChecked);
+  };
+
+  createEffect(() => {
+    if (!props.isPlaying) {
+      return;
+    }
+
+    const interval = setInterval(() => {
+      if (currentStep() >= 16) {
+        setCurrentStep(0);
+      }
+
+      const time = getAudioContext().currentTime;
+      if (drumsStore.kick.at(currentStep() + 1)) {
+        playKick(time);
+      }
+      if (drumsStore.snare.at(currentStep() + 1)) {
+        playSnare(time);
+      }
+      if (drumsStore.hihats.at(currentStep() + 1)) {
+        playHihats(time);
+      }
+
+      setCurrentStep((step) => step + 1);
+    }, 60_000 / props.bpm / 4);
+    // 1 minute is 60_000
+    // 60_000 / 4 gives us the interval between quarter notes
+    // we divide by 4 because we want 16th notes
+
+    setIntervalId(interval);
+
+    // Clean up any running timer when the BPM
+    // changes or the playback is stopped
+    onCleanup(() => {
+      clearInterval(intervalId());
+    });
+  });
+
   return (
     <div class={`${styles.wrapper} monospace`}>
       <div />
       {STEPS_ARRAY.map((step) => (
-        <div classList={{ [styles.step]: true, [styles.activeStep]: step === props.activeStep }}>
+        <div classList={{ [styles.step]: true, [styles.activeStep]: step === currentStep() }}>
           {step}
         </div>
       ))}
@@ -26,13 +74,13 @@ export default function DrumsSequencer(props: {
         <>
           <div class={styles.instrument}>{instrument}</div>
           {STEPS_ARRAY.map((step) => (
-            <div classList={{ [styles.activeStep]: props.isPlaying && step === props.activeStep }}>
+            <div classList={{ [styles.activeStep]: props.isPlaying && step === currentStep() }}>
               <input
                 type="checkbox"
-                checked={props.drumsStore?.[instrument][step]}
+                checked={drumsStore?.[instrument][step]}
                 onChange={(evt) => {
                   evt.preventDefault();
-                  props.onStepToggle(instrument, step + 1, evt.target.checked);
+                  onStepToggle(instrument, step + 1, evt.target.checked);
                 }}
               />
             </div>
