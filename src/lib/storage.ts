acf1fd5169c6d8c59496765f01fd4f97c75540be
Ivan Prignano <ivanprignano@gmail.com>
2025-11-07 13:31:29 +0100

    add storage module

---
+++ src/lib/storage.ts
@@ -0,0 +1,48 @@
+const STORAGE_KEY = 'taw_v0';
+
+type SerializedSong = {
+  n: string;
+  d: {
+    k: boolean[];
+    s: boolean[];
+    h: boolean[];
+  };
+  k: {
+    [step: number]: [freq: number, length: number][];
+  };
+};
+type TawStorage = { songs: SerializedSong[] };
+
+export const getSavedSongs = () => {
+  try {
+    const storage = localStorage.getItem(STORAGE_KEY);
+    if (storage) {
+      const parsedStorage: TawStorage = JSON.parse(storage);
+      return parsedStorage.songs;
+    }
+    return null;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+};
+
+export const saveSong = (name: string, serializedSong: Omit<SerializedSong, 'n'>) => {
+  try {
+    const savedSongs = getSavedSongs();
+    const newSong = { n: name, ...serializedSong };
+
+    if (!savedSongs) {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify({ songs: [newSong] }));
+      return true;
+    }
+
+    const newStorage = { songs: [...savedSongs, newSong] };
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(newStorage));
+
+    return true;
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
+};
