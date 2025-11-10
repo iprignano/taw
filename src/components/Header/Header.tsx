2554feee16f5cd1a93c03da1c6835105e33aa811
Ivan Prignano <ivanprignano@gmail.com>
2025-11-10 15:40:07 +0100

    add header save/load buttons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,29 @@
+import { createSignal } from 'solid-js';
+import SaveSongModal from '../SaveSongModal/SaveSongModal';
+import LoadSongModal from '../LoadSongModal/LoadSongModal';
 import styles from './styles.module.css';
 
 export default function Header() {
+  const [isSavingSong, setIsSavingSong] = createSignal(false);
+  const [isLoadingSong, setIsLoadingSong] = createSignal(false);
+
+  const onSaveClick = () => {
+    setIsSavingSong(true);
+  };
+  const onLoadClick = () => {
+    setIsLoadingSong(true);
+  };
+
   return (
     <header class={styles.header}>
-      <div class={styles.logo}>
-        <h1 class="monospace">&gt; toy_audio_workstation</h1>
-      </div>
+      <button class={styles.button} type="button" onClick={onSaveClick}>
+        Save
+      </button>
+      <button class={styles.button} type="button" onClick={onLoadClick}>
+        Load
+      </button>
+      {isSavingSong() && <SaveSongModal onClose={() => setIsSavingSong(false)} />}
+      {isLoadingSong() && <LoadSongModal onClose={() => setIsLoadingSong(false)} />}
     </header>
   );
 }
