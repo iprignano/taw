3590d7f979f17a071141c5338aa935cbab1d2766
Ivan Prignano <ivanprignano@gmail.com>
2025-11-10 11:09:05 +0100

    add close button to Modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,7 +1,8 @@
 import { onCleanup, onMount, useContext } from 'solid-js';
-import type { JSXElement } from 'solid-js';
-import styles from './styles.module.css';
 import { AppContext } from '../AppContext/AppContext';
+import { Close } from '../Icon/Close';
+import styles from './styles.module.css';
+import type { JSXElement } from 'solid-js';
 
 export default function Modal(props: { onClose(): void; children: JSXElement }) {
   const context = useContext(AppContext);
@@ -23,7 +24,12 @@ export default function Modal(props: { onClose(): void; children: JSXElement }) 
 
   return (
     <div id="overlay" class={styles.overlay} onClick={handleOverlayClick}>
-      <div class={styles.modal}>{props.children}</div>
+      <div class={styles.modal}>
+        <button class={styles.closeButton} onClick={() => props.onClose()}>
+          <Close />
+        </button>
+        {props.children}
+      </div>
     </div>
   );
 }
